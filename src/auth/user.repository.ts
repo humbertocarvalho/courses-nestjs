581f275c4a4ce05cf7cf8e0aedb2123204d61cc8
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-06-29 18:50:56 -0300

    Add duplicated username validation.

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,10 @@
 import { Repository, Entity, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -10,6 +14,16 @@ export class UserRepository extends Repository<User> {
     const user = new User();
     user.username = username;
     user.password = password;
-    await user.save();
+
+    try {
+      await user.save();
+    } catch (error) {
+      // Duplicated username
+      if (error.code == 23505) {
+        throw new ConflictException(`Username ${username} already exists.`);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
